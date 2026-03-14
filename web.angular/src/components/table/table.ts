d732ca47685931c71c49f1e5b339f2bc6346cab9
Tanvir Ather <tanvir.ather@zuhid.com>
2026-03-13 18:10:16 -0600

    Update on 2026-03-13

--- web.angular/src/components/table/table.ts
+++ web.angular/src/components/table/table.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, ContentChildren, Input, QueryList, TemplateRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ColTemplate } from './col-template';
 import { TableColumn } from './tableColumn';
 @Component({
   selector: 'nc-table',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table.html',
   styleUrl: './table.css',
   standalone: true,
@@ -12,11 +13,22 @@ import { TableColumn } from './tableColumn';
 export class Table {
   @Input() columns: TableColumn[] = [];
   @Input() dataList: any[] = [];
+  visibleDataList: any[] = [];
+
   @ContentChildren(ColTemplate) tableTemplates!: QueryList<ColTemplate>;
   cellTemplates = new Map<string, TemplateRef<any>>();
+  searchQuery: string = '';
 
   ngAfterContentInit(): void {
     this.cellTemplates.clear();
     this.tableTemplates.forEach(item => this.cellTemplates.set(item.key, item.tpl));
+    this.visibleDataList = this.dataList;
+  }
+
+  onSearch(): void {
+    // Implement search logic here, e.g., filter dataList based on searchQuery
+    this.visibleDataList = this.dataList.filter(item => Object.values(item).some(value =>
+      (value as string).toString().toLowerCase().includes(this.searchQuery.toLowerCase())
+    ));
   }
 }
