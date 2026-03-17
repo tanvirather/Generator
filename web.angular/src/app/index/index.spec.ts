5a030dc2dc0ec516dc09f0cb803ffec81a4742ef
Tanvir Ather <tanvir.ather@zuhid.com>
2026-03-16 20:24:56 -0600

    Update on 2026-03-16

--- web.angular/src/app/index/index.spec.ts
+++ web.angular/src/app/index/index.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { Index } from './index';
 
@@ -9,6 +10,7 @@ describe('Index', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Index],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Index);
